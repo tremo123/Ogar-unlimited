52e8b0dfa3cb49a3f777fb904d13831ef601ad36
Andrew54757 <andrews54757@gmail.com>
2016-01-28 16:48:41 -0500

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -921,6 +921,12 @@ gameServer.pfmsg = 1;
     },
     pause: function(gameServer,split) {
         gameServer.run = !gameServer.run; // Switches the pause state
+        if (!gameServer.run) {
+        gameServer.overideauto = true;
+    } else {
+        gameServer.overideauto = false;
+    }
+        console.log(gameServer.overideauto);
         var s = gameServer.run ? "Unpaused" : "Paused";
         console.log("[Console] " + s + " the game.");
     },
