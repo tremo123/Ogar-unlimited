2018acd57f8127f5ffb6f6b6a7c3f61a0555ce60
Andrew54757 <andrews54757@gmail.com>
2016-02-02 22:26:17 -0500

    Less flicker

--- src/GameServer.js
+++ src/GameServer.js
@@ -424,7 +424,7 @@ GameServer.prototype.liveconsole = function() {
         this.firstl = false;
     }
     
-    if (this.resticks > 11) {
+    if (this.resticks > 19) {
         this.firstl = true;
         this.resticks = 0;
     } else {
