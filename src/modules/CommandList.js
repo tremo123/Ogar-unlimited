e65d712828edaa9963bb41e72f286578648ad3a4
Andrew54757 <andrews54757@gmail.com>
2016-02-05 09:37:02 -0500

    Added shrink and enlarge commands

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -88,8 +88,60 @@ Commands.list = {
         console.log("[Console] Split      : Splits a player");
         console.log("[Console] Team       : Changes a players Team");
         console.log("[Console] Colortext  : changes text style");
+        console.log("[Console] Shrink     : Shrinks the game");
+        console.log("[Console] Enlarge    : Enlargens the game");
         console.log("[Console] ====================================================");
     },
+    enlarge: function(gameServer,split) {
+        borderDec = split[1];
+        if (isNaN(borderDec)) {
+            borderDec = 200;
+        }
+    gameServer.config.borderLeft -= borderDec;
+    gameServer.config.borderRight += borderDec;
+    gameServer.config.borderTop -= borderDec;
+    gameServer.config.borderBottom += borderDec;
+    
+        console.log("[Console] Successivly Enlarged game. Size: " + (gameServer.config.borderRight - gameServer.config.borderLeft) + "," + (gameServer.config.borderBottom - gameServer.config.borderTop));
+        
+    },
+    shrink: function(gameServer,split) {
+        borderDec = split[1];
+        if (isNaN(borderDec)) {
+            borderDec = 200;
+        }
+    gameServer.config.borderLeft += borderDec;
+    gameServer.config.borderRight -= borderDec;
+    gameServer.config.borderTop += borderDec;
+    gameServer.config.borderBottom -= borderDec;
+    
+    var len = gameServer.nodes.length;
+    for (var i = 0; i < len; i++) {
+        var node = gameServer.nodes[i];
+
+        if ((!node) || (node.getType() == 0)) {
+            continue;
+        }
+
+        // Move
+        if (node.position.x < gameServer.config.borderLeft) {
+            gameServer.removeNode(node);
+            i--;
+        } else if (node.position.x > gameServer.config.borderRight) {
+            gameServer.removeNode(node);
+            i--;
+        } else if (node.position.y < gameServer.config.borderTop) {
+            gameServer.removeNode(node);
+            i--;
+        } else if (node.position.y > gameServer.config.borderBottom) {
+            gameServer.removeNode(node);
+            i--;
+        }
+    }
+        console.log("[Console] Successivly shrinked game. Size: " + (gameServer.config.borderRight - gameServer.config.borderLeft) + "," + (gameServer.config.borderBottom - gameServer.config.borderTop));
+        
+    },
+    
     colortext: function(gameServer, split) {
         var c = split[1].toLowerCase();
         if (c == "red") {
