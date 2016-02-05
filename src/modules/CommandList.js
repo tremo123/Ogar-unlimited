0145ff5a8927c026d5fb54fe4d6bd52d43a4c890
Andrew54757 <andrews54757@gmail.com>
2016-02-04 22:41:41 -0500

    Make Live Console Compatible with Colortext Command

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -94,27 +94,41 @@ Commands.list = {
         var c = split[1].toLowerCase();
         if (c == "red") {
             console.log("\x1b[31mText is now Red");
+            gameServer.red = true;
         } else
         if (c == "green") {
             console.log("\x1b[32mText is now Green");
+            gameServer.green = true;
         } else
         if (c == "blue") {
             console.log("\x1b[34mText is now Blue");
+            gameServer.blue = true;
         } else
         if (c == "yellow") {
             console.log("\x1b[33mText is now Yellow");
+            gameServer.yellow = true;
         } else
         if (c == "reset") {
             console.log("\x1b[0mText is now Reset");
+            gameServer.red = false;
+            gameServer.green = false;
+            gameServer.blue = false;
+            gameServer.yellow = false;
+            gameServer.dim = false;
+            gameServer.bold = false;
+            gameServer.white = false;
         } else
         if (c == "bold") {
             console.log("\x1b[1mText is now Bold");
+            gameServer.bold = true;
         } else
         if (c == "white") {
             console.log("\x1b[37mText is now White");
+            gameServer.white = true;
         } else
         if (c == "dim") {
             console.log("\x1b[2mText is now Dim");
+            gameServer.dim = true;
         } else
         if (c == "help") {
             console.log("----- Colortext Help -----");
