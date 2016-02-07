9b8878da50620accf07af97f8f9abd1198ed35cd
Andrew54757 <andrews54757@gmail.com>
2016-02-07 12:53:57 -0500

    Update PlayerTracker.js

--- src/PlayerTracker.js
+++ src/PlayerTracker.js
@@ -24,7 +24,10 @@ function PlayerTracker(gameServer, socket) {
     this.team = 0;
     this.spectate = false;
     this.freeRoam = false; // Free-roam mode enables player to move in spectate mode
-
+this.massDecayMult = 1; // Anti-teaming multiplier
+   this.actionMult = 0; // If reaches over 1, it'll account as anti-teaming
+    this.actionDecayMult = 1; // Players not teaming will lose their anti-teaming multiplier far more quickly
+    
     // Viewing box
     this.sightRangeX = 0;
     this.sightRangeY = 0;
@@ -242,6 +245,26 @@ PlayerTracker.prototype.update = function() {
 };
 
 // Viewing box
+PlayerTracker.prototype.antiTeamTick = function() {
+     // ANTI-TEAMING DECAY
+     // Calculated even if anti-teaming is disabled.
+     this.actionMult *= (0.999 * this.actionDecayMult);
+     this.actionDecayMult *= 0.999;
+     
+     if (this.actionDecayMult > 1.002004) this.actionDecayMult = 1.002004; // Very small differences. Don't change this.
+     if (this.actionDecayMult < 1) this.actionDecayMult = 1;
+     
+     // Limit/reset anti-teaming effect
+     if (this.actionMult < 1 && this.massDecayMult > 1) this.actionMult = 0.6; // Speed up cooldown
+     if (this.actionMult > 1.4) this.actionMult = 1.4;
+     if (this.actionMult < 0.15) this.actionMult = 0;
+     
+     // Apply anti-teaming if required
+     if (this.actionMult > 1) this.massDecayMult = this.actionMult;
+     else this.massDecayMult = 1;
+ 
+ }
+ 
 
 PlayerTracker.prototype.updateSightRange = function() { // For view distance
     var totalSize = 1.0;
