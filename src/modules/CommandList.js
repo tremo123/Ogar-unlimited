7ecf9e6ac4a3877f9f8a2b06e2c05c626537e9a0
Andrew54757 <andrews54757@gmail.com>
2016-02-08 22:37:23 -0500

    Accidental overwite

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -92,6 +92,28 @@ Commands.list = {
         console.log("[Console] Enlarge    : Enlargens the game");
         console.log("[Console] ====================================================");
     },
+
+
+
+      resetateam: function(gameServer, split) {
+         // Validation checks
+         var id = parseInt(split[1]);
+          if (isNaN(id)) {
+            console.log("[Console] Please specify a valid player ID!");
+              return;
+        }
+ 
+          if (!gameServer.clients[id]) {
+            console.log("[Console] Client is nonexistent!");
+              return;
+        }
+ 
+         gameServer.clients[id].playerTracker.massDecayMult = 1;
+       gameServer.clients[id].playerTracker.actionMult = 0;
+         gameServer.clients[id].playerTracker.actionDecayMult = 1;
+         console.log("[Console] Successfully reset client's anti-team effect");
+     },
+
     enlarge: function(gameServer,split) {
         borderDec = split[1];
         if (isNaN(borderDec)) {
