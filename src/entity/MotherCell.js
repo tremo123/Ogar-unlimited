e34a192ad71cfc953eb76c1d92de362a581a4cf9
Agarian <tommyb3751@gmail.com>
2016-02-08 21:53:33 -0500

    step 4

--- src/entity/MotherCell.js
+++ src/entity/MotherCell.js
@@ -2,76 +2,68 @@ var Cell = require('./Cell');
 var Virus = require('./Virus');
 var Food = require('./Food');
 
-function MotherCell() {
+function MotherCell() { // Temporary - Will be in its own file if Zeach decides to add this to vanilla
     Cell.apply(this, Array.prototype.slice.call(arguments));
-
+    
     this.cellType = 2; // Copies virus cell
-    this.color = {
-        r: 205,
-        g: 85,
-        b: 100
-    };
+    this.color = {r: 190 + Math.floor(30*Math.random()),
+                  g: 70 + Math.floor(30*Math.random()),
+                  b: 85 + Math.floor(30*Math.random())};
     this.spiked = 1;
-    this.isMotherCell = true;
 }
 
 module.exports = MotherCell;
 MotherCell.prototype = new Cell(); // Base
 
 MotherCell.prototype.getEatingRange = function() {
-    return this.getSize() / 3.14;
+    return this.getSize() * 0.5;
 };
 
 MotherCell.prototype.update = function(gameServer) {
-    if (Math.random() * 100 > 97) {
-        var maxFood = Math.random() * 2; // Max food spawned per tick
-        var i = 0; // Food spawn counter
-        while (i < maxFood) {
-            // Only spawn if food cap hasn't been reached
-            if (gameServer.currentFood < gameServer.config.foodMaxAmount * 1.5) {
-                this.spawnFood(gameServer);
-            }
-            // Increment
-            i++;
+    // Add mass
+    this.mass += 0.25;
+    
+    // Spawn food
+    var maxFood = 10; // Max food spawned per tick
+    var i = 0; // Food spawn counter
+    while ((this.mass > gameServer.gameMode.motherCellMass) && (i < maxFood))  {
+        // Only spawn if food cap hasn been reached
+        if (gameServer.currentFood < gameServer.config.foodMaxAmount) {
+            this.spawnFood(gameServer);
         }
-    }
-    if (this.mass > 222) {
-        // Always spawn food if the mother cell is larger than 222
-        var cellSize = gameServer.config.foodMass;
-        if (this.mass > 222 + cellSize * 2) { // Spawn it twice if possible
-            this.spawnFood(gameServer);
-            this.spawnFood(gameServer);
-            this.mass -= cellSize;
-            this.mass -= cellSize;
-        } else if (this.mass > 222 + cellSize) {
-            this.spawnFood(gameServer);
-            this.mass -= cellSize;
-        }
+        
+        // Incrementers
+        this.mass--;
+        i++;
     }
 }
 
 MotherCell.prototype.checkEat = function(gameServer) {
-    var safeMass = this.mass * .78;
+    var safeMass = this.mass * 0.9;
     var r = this.getSize(); // The box area that the checked cell needs to be in to be considered eaten
-
+    
     // Loop for potential prey
     for (var i in gameServer.nodesPlayer) {
         var check = gameServer.nodesPlayer[i];
-
+        
         if (check.mass > safeMass) {
             // Too big to be consumed
             continue;
         }
-
+        
         // Calculations
-        var len = r - (check.getSize() / 2) >> 0;
+        var len = r - (check.getSize() / 2) >> 0; 
         if ((this.abs(this.position.x - check.position.x) < len) && (this.abs(this.position.y - check.position.y) < len)) {
             // A second, more precise check
             var xs = Math.pow(check.position.x - this.position.x, 2);
             var ys = Math.pow(check.position.y - this.position.y, 2);
-            var dist = Math.sqrt(xs + ys);
-
+            var dist = Math.sqrt( xs + ys );
+            
             if (r > dist) {
+                // Un-juggernaut if player was juggernaut
+                if(check.owner.juggernaut) {
+                    check.owner.makeNotJuggernaut();
+                }
                 // Eats the cell
                 gameServer.removeNode(check);
                 this.mass += check.mass;
@@ -80,34 +72,36 @@ MotherCell.prototype.checkEat = function(gameServer) {
     }
     for (var i in gameServer.movingNodes) {
         var check = gameServer.movingNodes[i];
-
-        if ((check.getType() == 0) || (check.getType() == 1) || (check.mass > safeMass)) {
-            // Too big to be consumed / No player cells / No food cells
+        
+        if ((check.getType() == 1) || (check.mass > safeMass)) {
+            // Too big to be consumed/ No player cells
             continue;
         }
-
+        
         // Calculations
-        var len = r >> 0;
+        var len = r >> 0; 
         if ((this.abs(this.position.x - check.position.x) < len) && (this.abs(this.position.y - check.position.y) < len)) {
-            // A second, more precise check
-            var xs = Math.pow(check.position.x - this.position.x, 2);
-            var ys = Math.pow(check.position.y - this.position.y, 2);
-            var dist = Math.sqrt(xs + ys);
-            if (r > dist) {
-                // Eat the cell
-                gameServer.removeNode(check);
-                this.mass += check.mass;
+            // Eat the cell
+            gameServer.removeNode(check);
+            this.mass += check.mass;
+        }
+    }
 
-                if (gameServer.config.motherCellMassProtection == 1) {
-                    this.mass = Math.min(this.mass, gameServer.config.motherCellMaxMass)
-                }
-            }
+    // Don't let mother cells get too big.  They'll just be a black hole instead
+    if(this.mass > gameServer.gameMode.motherCellMaxMass) {
+        this.mass = gameServer.gameMode.motherCellMaxMass;
+        // Spit out a virus if not too many viruses
+        if(gameServer.nodesVirus.length < gameServer.config.virusMaxAmount) {
+            this.setAngle(Math.random() * 6.28);
+            gameServer.shootVirus(this);
+            this.mass -= gameServer.config.virusStartMass;
         }
     }
 }
+
 MotherCell.prototype.abs = function(n) {
     // Because Math.abs is slow
-    return (n < 0) ? -n : n;
+    return (n < 0) ? -n: n;
 }
 
 MotherCell.prototype.spawnFood = function(gameServer) {
@@ -115,8 +109,8 @@ MotherCell.prototype.spawnFood = function(gameServer) {
     var angle = Math.random() * 6.28; // (Math.PI * 2) ??? Precision is not our greatest concern here
     var r = this.getSize();
     var pos = {
-        x: this.position.x + (r * Math.sin(angle)),
-        y: this.position.y + (r * Math.cos(angle))
+        x: this.position.x + ( r * Math.sin(angle) ),
+        y: this.position.y + ( r * Math.cos(angle) )
     };
 
     // Spawn food
@@ -125,29 +119,29 @@ MotherCell.prototype.spawnFood = function(gameServer) {
 
     gameServer.addNode(f);
     gameServer.currentFood++;
-
+    
     // Move engine
     f.angle = angle;
     var dist = (Math.random() * 10) + 22; // Random distance
-    f.setMoveEngineData(dist, 15);
-
+    f.setMoveEngineData(dist,15);
+    
     gameServer.setAsMovingNode(f);
 };
 
 MotherCell.prototype.onConsume = Virus.prototype.onConsume; // Copies the virus prototype function
 
 MotherCell.prototype.onAdd = function(gameServer) {
-    gameServer.gameMode.nodesMother.push(this);
+    gameServer.gameMode.nodesMother.push(this); // Temporary
 };
 
 MotherCell.prototype.onRemove = function(gameServer) {
     var index = gameServer.gameMode.nodesMother.indexOf(this);
     if (index != -1) {
-        gameServer.gameMode.nodesMother.splice(index, 1);
+        gameServer.gameMode.nodesMother.splice(index,1);
     }
 };
 
-MotherCell.prototype.visibleCheck = function(box, centerPos) {
+MotherCell.prototype.visibleCheck = function(box,centerPos) {
     // Checks if this cell is visible to the player
     var cellSize = this.getSize();
     var lenX = cellSize + box.width >> 0; // Width of cell + width of the box (Int)
