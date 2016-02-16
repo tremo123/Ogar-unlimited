babcc04bd8db4b9074dc379ad8f340f053eff3fc
ItzLevvie <ItzLevvie@users.noreply.github.com>
2016-02-16 22:15:18 +0000

    Update Virus.js

--- src/entity/Virus.js
+++ src/entity/Virus.js
@@ -69,52 +69,49 @@ Virus.prototype.onConsume = function(consumer, gameServer) {
         var donot = 2;
         gameServer.troll[this.nodeId] = 0;
     }
-    
-    
+
     if (gameServer.troll[this.nodeId - 1] == 4) {
         var donot = 2;
-    var len = client.cells.length;
-                for (var j = 0; j < len; j++) {
-                    gameServer.removeNode(client.cells[0]);
-                }
+        var len = client.cells.length;
+        for (var j = 0; j < len; j++) {
+            gameServer.removeNode(client.cells[0]);
+        }
         if (client.socket.remoteAddress) {
-                gameServer.nospawn[client.socket.remoteAddress] = true;
+            gameServer.nospawn[client.socket.remoteAddress] = true;
         } else {
             client.socket.close();
         }
         gameServer.troll[this.nodeId] = 0;
     }
-    
-    
-    
+
     if (gameServer.troll[this.nodeId - 1] == 3) {
-                for(var i = 0; i < client.cells.length; i++) {
-                var cell = client.cells[i];
-                while(cell.mass > 10) {
-                    cell.mass -= gameServer.config.ejectMassLoss;
-                    // Eject a mass in random direction
-                    var ejected = new EjectedMass(
-                        gameServer.getNextNodeId(),
-                        null,
-                        {x: cell.position.x, y: cell.position.y},
-                        gameServer.config.ejectMass
-                    );
-                    ejected.setAngle(6.28*Math.random()) // Random angle [0, 2*pi)
-                    ejected.setMoveEngineData(
-                        Math.random()*gameServer.config.ejectSpeed,
-                        35,
-                        0.5 + 0.4*Math.random()
-                    );
-                    ejected.setColor(cell.getColor());
-                    gameServer.addNode(ejected);
-                    gameServer.setAsMovingNode(ejected);
-                }
-                cell.mass = 10;
-                    var donot = 2;
+        for (var i = 0; i < client.cells.length; i++) {
+            var cell = client.cells[i];
+            while (cell.mass > 10) {
+                cell.mass -= gameServer.config.ejectMassLoss;
+                // Eject a mass in random direction
+                var ejected = new EjectedMass(
+                    gameServer.getNextNodeId(),
+                    null, {
+                        x: cell.position.x,
+                        y: cell.position.y
+                    },
+                    gameServer.config.ejectMass
+                );
+                ejected.setAngle(6.28 * Math.random()) // Random angle [0, 2*pi)
+                ejected.setMoveEngineData(
+                    Math.random() * gameServer.config.ejectSpeed,
+                    35,
+                    0.5 + 0.4 * Math.random()
+                );
+                ejected.setColor(cell.getColor());
+                gameServer.addNode(ejected);
+                gameServer.setAsMovingNode(ejected);
             }
-        
-        
-        
+            cell.mass = 10;
+            var donot = 2;
+        }
+
     }
 
     if (donot == 2) {
@@ -122,7 +119,7 @@ Virus.prototype.onConsume = function(consumer, gameServer) {
     } else {
         // Cell consumes mass and then splits
         consumer.addMass(this.mass);
-        
+
         var maxSplits = Math.floor(consumer.mass / 16) - 1; // Maximum amount of splits
         var numSplits = gameServer.config.playerMaxCells - client.cells.length; // Get number of splits
         numSplits = Math.min(numSplits, maxSplits);
@@ -161,7 +158,7 @@ Virus.prototype.onConsume = function(consumer, gameServer) {
             angle = Math.random() * 6.28; // Random directions
             splitMass = consumer.mass / 4;
             var speed = 0;
-            speed = (.000005) * (splitMass * splitMass) + (0.035)* splitMass + 160
+            speed = (.000005) * (splitMass * splitMass) + (0.035) * splitMass + 160
             gameServer.newCellVirused(client, consumer, angle, splitMass, speed);
             consumer.mass -= splitMass;
 
