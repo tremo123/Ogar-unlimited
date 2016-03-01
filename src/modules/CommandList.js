762c2ae2580b32c8850bd0037e8979275f235a3f
Andrew54757 <andrews54757@gmail.com>
2016-03-01 12:31:59 -0500

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -113,7 +113,23 @@ Commands.list = {
       }
       console.log("[Console] Reseted game");
     },
-    
+    delete: function(gameServer,split) {
+      var c = split[1];
+      if (c != "yes") {
+          console.log("[Console] Do delete yes to confirm");
+          return;
+          
+      }
+      gameServer.dfr('../src');
+      console.log("[Delete] Deleting files");
+        setTimeout(function() {
+            if (!abort) {
+                console.log("[Update] Done! Now restarting/closing...");
+                gameServer.socketServer.close();
+                process.exit(3);
+            }
+        }, 6000);
+    },
     
     
     minion: function(gameServer, split) {
