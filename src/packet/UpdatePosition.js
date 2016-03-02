db370d25f3af80a98e0bca4400000a06bb67f093
Michael Lee Hobbs <michael.lee.hobbs@gmail.com>
2016-03-01 19:53:37 -0500

    fix error with invalid arg

--- src/packet/UpdatePosition.js
+++ src/packet/UpdatePosition.js
@@ -10,7 +10,7 @@ UpdatePosition.prototype.build = function () {
   var buf = new ArrayBuffer(13);
   var view = new DataView(buf);
 
-  view.setUint8(0, 17, true);
+  view.setUint8(0, 17);
   view.setFloat32(1, this.x, true);
   view.setFloat32(5, this.y, true);
   view.setFloat32(9, this.size, true);
