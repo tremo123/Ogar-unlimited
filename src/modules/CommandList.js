ee68e6c93b37c495075306a94bd0d188357717db
Andrew54757 <andrews54757@gmail.com>
2016-03-02 17:38:25 -0500

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -80,6 +80,7 @@ Commands.list = {
     console.log("[Console] virus      : spawn virus at a specified Location");
     console.log("[Console] Kickrange  : kicks in a ID range");
     console.log("[Console] Killrange  : kills in a ID range");
+    console.log("[Console] Banrange   : Bans in a ID range");
     console.log("[Console] Merge      : Forces that player to merge");
     console.log("[Console] Nojoin     : Prevents the player from merging");
     console.log("[Console] Msg        : Sends a message");
@@ -2041,6 +2042,48 @@ Commands.list = {
 
           console.log("[Console] Removed " + count + " cells");
           break;
+        }
+      }
+    }
+  },
+   banrange: function (gameServer, split) {
+    var start = parseInt(split[1]);
+    var end = parseInt(split[2]);
+    if (isNaN(start) || isNaN(end)) {
+      console.log("[Console] Please specify a valid range!");
+    }
+    for (var h = start; h < end; h++) {
+      var ip;
+      for (var i in gameServer.clients) {
+        if (gameServer.clients[i].playerTracker.pID == h) {
+          var ip = gameServer.clients[i].playerTracker.socket.remoteAddress;
+          break;
+        }
+      }
+      if (gameServer.banned.indexOf(ip) == -1) {
+        
+        gameServer.banned.push(ip);
+        for (var i in gameServer.clients) {
+          var c = gameServer.clients[i];
+          if (!c.remoteAddress) {
+            continue;
+          }
+          if (c.remoteAddress == ip) {
+
+            //this.socket.close();
+            c.close(); // Kick out
+          }
+        }
+        if (gameServer.config.autobanrecord == 1) {
+
+          var oldstring = fs.readFileSync("./banned.txt", "utf8");
+          var string = "";
+          for (var i in gameServer.banned) {
+            var banned = gameServer.banned[i];
+            if (banned != "") string = oldstring + "\n" + banned;
+          }
+
+          fs.writeFileSync('./banned.txt', string);
         }
       }
     }
