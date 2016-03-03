1fe39d169c1972a6180f18348eda3b1f14bfc967
Michael Lee Hobbs <michael.lee.hobbs@gmail.com>
2016-03-03 18:27:48 -0500

    commented use of readFileSync

--- src/ai/minionLoader.js
+++ src/ai/minionLoader.js
@@ -23,6 +23,7 @@ MinionLoader.prototype.loadNames = function () {
     var fs = require("fs"); // Import the util library
 
     // Read and parse the names - filter out whitespace-only names
+    // fs.readFileSync is only used during server start
     this.randomNames = fs.readFileSync("./botnames.txt", "utf8").split(/[\r\n]+/).filter(function (x) {
       return x != ''; // filter empty names
     });
