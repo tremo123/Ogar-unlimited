3617286333cc4482553ee6a23ae9a917594eb714
Andrew54757 <andrews54757@gmail.com>
2016-03-05 22:14:58 -0500

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -153,9 +153,11 @@ Commands.list = {
   },
   
   reset: function (gameServer, split) {
+for (var j = 0; j < 10; j++) {
     for (var i in gameServer.nodes) {
       gameServer.removeNode(gameServer.nodes[i]);
     }
+}
     console.log("[Console] Reseted game");
   },
   delete: function (gameServer, split) {
