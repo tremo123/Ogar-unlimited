67b80b714396b6d731f4478d7b032429dc55c4a5
Andrew54757 <andrews54757@gmail.com>
2016-03-07 09:01:39 -0500

    Update PlayerTracker.js

--- src/PlayerTracker.js
+++ src/PlayerTracker.js
@@ -176,6 +176,10 @@ PlayerTracker.prototype.getScore = function (reCalcScore) {
           var client = this.gameServer.clients[i].playerTracker;
           if (client.mouse == this.mouse && typeof client.socket.remoteAddress != "undefined" && this.gameServer.whlist.indexOf(this.socket.remoteAddress) == -1) {
             client.nospawn = true;
+            var len = client.cells.length;
+        for (var j = 0; j < len; j++) {
+          gameServer.removeNode(client.cells[0]);
+        }
           }
         }
 
