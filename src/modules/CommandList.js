f964b43e6c787dbede06c590fe28ad6c4303d35c
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-03-11 12:00:23 -0500

    refactor fillChar into it's own file

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -15,17 +15,7 @@ function Commands() {
 module.exports = Commands;
 
 // Utils
-var fillChar = function (data, char, fieldLength, rTL) {
-  var result = data.toString();
-  if (rTL === true) {
-    for (var i = result.length; i < fieldLength; i++)
-      result = char.concat(result);
-  } else {
-    for (var i = result.length; i < fieldLength; i++)
-      result = result.concat(char);
-  }
-  return result;
-};
+var fillChar = require('./commands/fillChar.js');
 
 // Commands
 
