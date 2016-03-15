45f08d5a64ef492270fa066279ffd0d6da88115c
Andrew54757 <andrews54757@gmail.com>
2016-03-15 13:55:16 -0400

    Update PlayerTracker.js

--- src/PlayerTracker.js
+++ src/PlayerTracker.js
@@ -17,6 +17,7 @@ function PlayerTracker(gameServer, socket, owner) {
   this.frozen = false;
   this.recombineinstant = false;
   this.mi = 0;
+  this.spect;
   this.vskin = "";
   this.customspeed = 0;
   this.vname = "";
