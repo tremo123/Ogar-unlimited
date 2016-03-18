c8ec6064efcb224add28363724a6051a79ea6c29
Hassan <hassanila97@users.noreply.github.com>
2016-03-18 15:36:51 +0100

    Update Install Dependencies (Windows).bat
    Added check if node is installed and also enable users to update dependencies by re-running it

--- src/Install Dependencies (Windows).bat
+++ src/Install Dependencies (Windows).bat
@@ -1,6 +1,52 @@
 @echo off
-title Install Dependencies
-cd /d %~dp0
+
+where node >nul
+if %ERRORLEVEL% NEQ 0 (
+
+    set SETUP_DIR=%CD%
+
+    echo NodeJs is not installed on this computer...
+    echo.
+    echo Press any key to download NodeJs...
+    pause >nul 2<&1
+
+if not exist "%systemdrive%\Program Files (x86)" (
+    start https://nodejs.org/dist/v5.6.0/node-v5.6.0-x86.msi
+) else (
+    start https://nodejs.org/dist/v5.6.0/node-v5.6.0-x64.msi
+)
+
+echo.
+echo.
+echo Please open the downloaded file and follow the instructions when it's done downloading...
+echo Reopen this batch after you have installed NodeJs...
+echo.
+echo.
+echo Press any key to exit...
+pause >nul 2<&1
+exit
+
+) else (
+
+if exist "node_modules/." (
+echo.
+echo Dependencies already installed...
+echo.
+echo.
+set /p input="Do you want to update? (Y/N)"
+cls
+if "%input%" == "N" if "%input%" == "n" (
+echo.
+echo Press any key to exit...
+pause >nul
+)
+rd /s /q "node_modules/"
+)
+
+echo.
+echo.
+echo Clearing cache... Please wait...
+call npm cache clean
 cls
 echo.
 echo.
@@ -9,7 +55,13 @@ call npm install
 cls
 echo.
 echo.
+echo npm installed successfully...
+
+cls
+echo.
+echo.
 echo All dependencies installed successfully...
 echo.
 echo Press any key to exit...
-pause >nul 2<&1
+pause >nul
+)
