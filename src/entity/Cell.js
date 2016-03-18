480e4b40cc8d6aa0ad28c9fa3d8b262f2577d0a2
Andrew54757 <andrews54757@gmail.com>
2016-03-18 12:52:46 -0400

    Update Cell.js

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -160,7 +160,7 @@ Cell.prototype.collisionCheck2 = function (objectSquareSize, objectPosition) {
 
   var dx = this.position.x - objectPosition.x;
   var dy = this.position.y - objectPosition.y;
-if (this.owner.gameServer.config.playerRecombineTime == 0) {
+if (this.gameServer.config.playerRecombineTime == 0) {
   return (dx * dx + dy * dy + this.getSquareSize() <= objectSquareSize);
 } else {
   return (dx * dx + dy * dy <= objectSquareSize);
