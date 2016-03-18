17a7c64edbee78e1570bfd108e0c448f4b63536f
Andrew54757 <andrews54757@gmail.com>
2016-03-18 15:24:15 -0400

    Update Cell.js

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -1,6 +1,5 @@
 Cell.spi = 0;
 Cell.virusi = 255;
-
 function Cell(nodeId, owner, position, mass, gameServer) {
   this.nodeId = nodeId;
   this.owner = owner; // playerTracker that owns this cell
@@ -160,7 +159,7 @@ Cell.prototype.collisionCheck2 = function (objectSquareSize, objectPosition) {
 
   var dx = this.position.x - objectPosition.x;
   var dy = this.position.y - objectPosition.y;
-if (this.gameServer.config.playerRecombineTime == 0) {
+if (1 == 0) {
   return (dx * dx + dy * dy + this.getSquareSize() <= objectSquareSize);
 } else {
   return (dx * dx + dy * dy <= objectSquareSize);
