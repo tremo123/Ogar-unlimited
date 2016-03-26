e524046802ecb9ba19f11acd8ddee45726ff37af
NatsuTheGreat <ry4n.berd@gmail.com>
2016-03-25 19:06:41 -0500

    Actually use the autosplit function in GameServer.js.

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -79,7 +79,7 @@ Cell.prototype.addMass = function(n) {
         if (this.mass + n > this.owner.gameServer.config.playerMaxMass && this.owner.cells.length < this.owner.gameServer.config.playerMaxCells) {
             this.mass = (this.mass + n) / 2;
             var randomAngle = Math.random() * 6.28; // Get random angle
-            this.owner.gameServer.newCellVirused(this.owner, this, randomAngle, this.mass, 350);
+            this.owner.gameServer.autoSplit(this.owner, this, randomAngle, this.mass, 350);
         } else {
             this.mass += n;
             var th = this;
