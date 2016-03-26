8f7ca31cc6ac51dd8a29165f2f407b464fdea7dd
Andrew54757 <andrews54757@gmail.com>
2016-03-26 14:27:34 -0400

    Update Cell.js

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -12,7 +12,7 @@ function Cell(nodeId, owner, position, mass, gameServer) {
     };
     this.position = position;
     this.visible = true;
-    this.name;
+    this.name = '';
     this.mass = mass; // Starting mass of the cell
     this.cellType = -1; // 0 = Player Cell, 1 = Food, 2 = Virus, 3 = Ejected Mass
     this.spiked = Cell.spi; // If 1, then this cell has spikes around it
