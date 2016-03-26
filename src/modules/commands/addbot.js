f8160bddc16c4a23d6d16ae0f0af34cf2be23c1d
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-03-26 07:18:55 -0400

    added todo

--- src/modules/commands/addbot.js
+++ src/modules/commands/addbot.js
@@ -1,5 +1,5 @@
-module.exports = function (gameServer, split) {
-  var add = parseInt(split[1]);
+module.exports = function (gameServer, args) {
+  var add = parseInt(args[1]);
   if (isNaN(add)) {
     add = 1; // Adds 1 bot if user doesnt specify a number
   }
@@ -7,6 +7,7 @@ module.exports = function (gameServer, split) {
   gameServer.liveticks = 0;
   for (var i = 0; i < add; i++) {
     gameServer.bots.addBot();
+    // todo encapsulation
     gameServer.sbo++;
   }
   console.log("[Console] Added " + add + " player bots");
