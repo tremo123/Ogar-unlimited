3f60f58deada51ebbb57b8f312096d2b17ae7a82
Andrew54757 <andrews54757@gmail.com>
2016-03-30 17:49:41 -0400

    Update index.js

--- src/plugins/example/index.js
+++ src/plugins/example/index.js
@@ -1,14 +1,16 @@
 'use strict';
 var plugin = [];
-plugin.name = ""; // Name of plugin where it would be listed in
-plugin.author = "andrews54757"; // author
 plugin.command = []; // extra commands
 plugin.commandName = []; // extra commands
 plugin.addToHelp = [];
-plugin.commandName[0] = "tptoplayer";
-plugin.addToHelp[0] = "tptoplayer : tps to a player";
 plugin.gamemodeId = [];
 plugin.gamemode = [];
+
+
+plugin.name = ""; // Name of plugin where it would be listed in
+plugin.author = "andrews54757"; // author
+plugin.commandName[0] = "tptoplayer";
+plugin.addToHelp[0] = "tptoplayer : tps to a player";
 plugin.version = '1.0.0';
 plugin.compatVersion = ''; // compatable with (todo)
 plugin.gamemodeId[0] = 80;
