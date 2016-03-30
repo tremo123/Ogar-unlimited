940cb57afff955fd688d20e5ef779b1385b63611
Andrew54757 <andrews54757@gmail.com>
2016-03-30 16:18:43 -0400

    Update example.js

--- src/plugins/example.js
+++ src/plugins/example.js
@@ -4,6 +4,7 @@ plugin.name = ""; // Name of plugin where it would be listed in
 plugin.author = "andrews54757"; // author
 plugin.command = []; // extra commands
 plugin.commandName = []; // extra commands
+plugin.addToHelp = [];
 plugin.commandName[0] = "tptoplayer";
 plugin.addToHelp[0] = "tptoplayer : tps to a player";
 plugin.gamemodeId = []; // todo with plugin gamemodes
@@ -12,6 +13,8 @@ plugin.version = '1.0.0';
 plugin.compatVersion = ''; // compatable with (todo)
 plugin.init = function (gameServer) {
   // init, Used to do stuff such as overriding things
+  
+  
   
 }
 plugin.command[0] = function(gameServer,split) {
