355253b2bc7f1623560f3d28b46e9a2dd5a08587
Andrew54757 <andrews54757@gmail.com>
2016-03-30 15:40:11 -0400

    max mass is back!

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -105,7 +105,8 @@ Cell.prototype.addMass = function (n) {
   } else {
 
         if (this.mass + n > this.owner.gameServer.config.playerMaxMass && this.owner.cells.length < this.owner.gameServer.config.playerMaxCells) {
-            this.mass = (this.mass + n) / 2;
+            this.mass = this.mass + n;
+            this.mass = this.mass / 2;
              var randomAngle = Math.random() * 6.28; // Get random angle
              this.owner.gameServer.autoSplit(this.owner, this, randomAngle, this.mass, 350);
          } else {
