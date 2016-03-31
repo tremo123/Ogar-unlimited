841da3ee3d1e18c5a5032bdfcf3006e05a469017
Andrew54757 <andrews54757@gmail.com>
2016-03-31 16:47:08 -0400

    Update PluginLoader.js

--- src/core/PluginLoader.js
+++ src/core/PluginLoader.js
@@ -33,6 +33,7 @@ this.gameServer = gameServer
       for (var i in files) {
         
       var plugin = require('../plugins/' + files[i] + '/index.js');
+  if (plugin.name && plugin.author && plugin.version && plugin.init) {
         this.plugins[plugin.name] = plugin;
         plugin.init(this.gameServer);
         if (this.plugins) {
@@ -49,12 +50,16 @@ this.gameServer = gameServer
             }
           
         }
+
         
       }
       
         console.log("[Console] loaded plugin: " + plugin.name + " By " + plugin.author + " version " + plugin.version);
         
-      }
+      } else {
+console.log("[Console] Didnt load pluginfile " + files[i] + " because it was missing parameters");
+}
+}
       
     } else {
     
@@ -67,7 +72,8 @@ this.gameServer = gameServer
         
         try {
       var plugin = require('../plugins/' + files[i] + '/index.js');
-        this.plugins[files[i]] = plugin;
+          if (plugin.name && plugin.author && plugin.version && plugin.init) {
+this.plugins[files[i]] = plugin;
         plugin.init(this.gameServer);
         if (this.plugins) {
           if (plugin.commandName) {
@@ -87,7 +93,9 @@ this.gameServer = gameServer
       }
       
         console.log("[Console] loaded plugin: " + plugin.name + " By " + plugin.author + " version " + plugin.version);
-        
+        } else {
+console.log("[Console] Didnt load pluginfile " + files[i] + " because it was missing parameters");
+}
       } catch (e) {
         console.log("[Console] Failed to load pluginfile " + files[i] + " Reason: " + e);
         
