57d773f69e305ae623d711446c94b0429835076c
Andrew54757 <andrews54757@gmail.com>
2016-04-05 21:17:15 -0400

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -118,6 +118,7 @@ Commands.list = {
     }
     console.log("[Console] ====================================================");
   },
+  plugin: require(''./commands/plugin.js'),
   blind: require('./commands/blind.js'),
   pcmd: require('./commands/pcmd.js'),
   reset: require('./commands/reset.js'),
