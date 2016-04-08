8e04071ab7ef76668974a89ae72ff7711124f0b4
Andrew54757 <andrews54757@gmail.com>
2016-04-08 10:25:29 -0400

    Update Updater.js

--- src/core/Updater.js
+++ src/core/Updater.js
@@ -8,7 +8,7 @@ const exec = require('child_process').exec;
 
 module.exports = class Updater {
   constructor(gameServer) {
-    this.url = "http://raw.githubusercontent.com/AJS-development/Ogar-unlimited/" + "dev" + "/";
+    this.url = "http://raw.githubusercontent.com/AJS-development/Ogar-unlimited/" + "master" + "/";
     this.gameServer = gameServer;
     this.files = require(path.resolve(process.cwd(), 'files.json'));
     this.newFiles = {};
