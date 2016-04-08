f9d13643f0a7a01ebc6f4019ef9c3cd2859ff189
Andrew54757 <andrews54757@gmail.com>
2016-04-08 16:05:25 -0400

    Update reset.js

--- src/modules/commands/reset.js
+++ src/modules/commands/reset.js
@@ -1,10 +1,10 @@
 'use strict';
 // todo this needs review
 module.exports = function (gameServer, split) {
-  let nodes = gameServer.getWorld().getNodes();
+  let nodes = gameServer._nodes;
   for (var j = 0; j < 10; j++) {
     for (var i in nodes) {
-      gameServer.getWorld().removeNode(nodes[i]);
+      gameServer.removeNode(nodes[i]);
     }
   }
   console.log("[Console] Reset the game");
