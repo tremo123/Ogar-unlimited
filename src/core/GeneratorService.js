8e436a354cdc4ecd993c1de043d9577ccd06867f
Andrew54757 <andrews54757@gmail.com>
2016-04-09 07:16:08 -0400

    Update GeneratorService.js

--- src/core/GeneratorService.js
+++ src/core/GeneratorService.js
@@ -35,7 +35,7 @@ module.exports = class GeneratorService {
     let f = new Entity.Food(this.gameServer.getWorld().getNextNodeId(), null, utilities.getRandomPosition(this.config.borderRight, this.config.borderLeft, this.config.borderBottom, this.config.borderTop), this.config.foodMass, this.gameServer);
     f.setColor(utilities.getRandomColor());
 
-    this.dataBase.put(f.toJSON());
+
 
     this.gameServer.addNode(f);
     this.gameServer.currentFood++;
