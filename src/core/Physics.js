b80bc949a4ad0b8a6bf09b8c4cc5fa0f0745a4f3
Andrew54757 <andrews54757@gmail.com>
2016-04-14 16:14:16 -0400

    Update Physics.js

--- src/core/Physics.js
+++ src/core/Physics.js
@@ -74,7 +74,6 @@ module.exports = class Physics {
 
       // Add to moving cells list
       gameServer.addNode(split, "moving");
-       gameServer.getWorld().addNode(split, 'player');
       splitCells++;
     });
     if (splitCells > 0) player.actionMult += 0.5; // Account anti-teaming
