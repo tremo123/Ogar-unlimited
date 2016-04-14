fed2a614a7d3f41240b6514f1a3df77d671019fc
Andrew54757 <andrews54757@gmail.com>
2016-04-14 16:05:03 -0400

    Update killall.js

--- src/modules/commands/killall.js
+++ src/modules/commands/killall.js
@@ -1,11 +1,9 @@
 'use strict';
 module.exports = function (gameServer, split) {
-var count = 0;
-    var len = gameServer._nodesPlayer.length;
-    for (var i = 0; i < len; i++) {
-      gameServer.removeNode(gameServer._nodesPlayer[i]);
-      if (gameServer._nodesPlayer[i]) count++;
-    }
-    gameServer._nodesPlayer = [];
-    console.log("[Console] Removed " + count + " cells");
+  let count = 0;
+   gameServer.getWorld().getNodes('player').forEach((node)=> {
+    gameServer.removeNode(node);
+     count++;
+  });
+   console.log("[Console] Removed " + count + " cells");
 };
