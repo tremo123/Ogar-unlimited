f3a0f9901d6012141d8fedf946eabddc343649b3
Andrew54757 <andrews54757@gmail.com>
2016-04-16 13:50:36 -0400

    Update killall.js

--- src/modules/commands/killall.js
+++ src/modules/commands/killall.js
@@ -2,7 +2,7 @@
 module.exports = function (gameServer, split) {
   let count = 0;
    for (var i in gameServer.clients) {
-     client = gameServer.clients[i];
+     var client = gameServer.clients[i];
      client.cells.forEach((cell)=>gameServer.removeNode(cell));
      
    }
