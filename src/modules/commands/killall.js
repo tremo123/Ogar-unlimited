23659a9e62009da1d36cd76ca9118f10a186fc02
Andrew54757 <andrews54757@gmail.com>
2016-04-16 13:54:27 -0400

    Update killall.js

--- src/modules/commands/killall.js
+++ src/modules/commands/killall.js
@@ -3,8 +3,12 @@ module.exports = function (gameServer, split) {
   let count = 0;
    for (var i in gameServer.clients) {
      var client = gameServer.clients[i];
-     client.playerTracker.cells.forEach((cell)=>gameServer.removeNode(cell));
-     count++;
+     client.playerTracker.cells.forEach((cell)=>{
+         gameServer.removeNode(cell);
+         count++;
+         
+     }
+         );
    }
    console.log("[Console] Removed " + count + " cells");
 };
