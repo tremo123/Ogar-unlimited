d638efce3b5c9c2c7936f5ba7bef8ee06398d227
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-04-17 07:42:32 -0400

    fixed killall bug #413

--- src/modules/commands/killall.js
+++ src/modules/commands/killall.js
@@ -1,14 +1,11 @@
 'use strict';
+const Kill = require('./kill.js');
+
 module.exports = function (gameServer, split) {
   let count = 0;
-   for (var i in gameServer.clients) {
-     var client = gameServer.clients[i];
-     client.playerTracker.cells.forEach((cell)=>{
-         gameServer.removeNode(cell);
-         count++;
-         
-     }
-         );
-   }
-   console.log("[Console] Removed " + count + " cells");
+  gameServer.clients.forEach((client)=>{
+    count++;
+    Kill(gameServer, ['', client.playerTracker.pID])
+  });
+  console.log("[Console] Removed " + count + " cells");
 };
