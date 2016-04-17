2365dff3e3dbddef30b716fade2ff97613a21833
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-04-17 09:11:06 -0400

    testing new build.js for travis

--- src/build.js
+++ src/build.js
@@ -3,6 +3,22 @@ var GameServer = require('./core/GameServer');
 
 var gameServer = new GameServer();
 
+const VERSION = 'buildTest';
+const ControlServer = require('./core/ControlServer');
+let controlServer = new ControlServer(VERSION);
+
+// There is no stopping an exit so clean up
+// NO ASYNC CODE HERE - only use SYNC or it will not happen
+process.on('exit', (code) => {
+  console.log("OgarUnlimited terminated with code: " + code);
+  controlServer.stop();
+});
+
+// init/start the control server
+controlServer.init();
+controlServer.start();
+
+
 setTimeout(function () {
   process.exit(0);
 }, 60000);
