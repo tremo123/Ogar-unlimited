b82b9e7513fb4be6f9f45b04e02c5d65f66f9a9a
Andrew54757 <andrews54757@gmail.com>
2016-04-23 15:29:39 -0400

    Update index.js

--- src/entity/index.js
+++ src/entity/index.js
@@ -4,4 +4,9 @@ module.exports = {
   Food: require('./Food'),
   Virus: require('./Virus'),
   EjectedMass: require('./EjectedMass'),
+  MovingVirus: require('./MovingVirus.js'),
+  MovingCell: require('./MovingCell.js'),
+  StickyCell: require('./StickyCell.js'),
+  MotherCell: require('./MotherCell.js'),
+  Beacon: require('./Beacon.js')
 };
