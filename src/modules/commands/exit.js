0cef17e8677f3abc2a18fded5bbf09885e75f5f5
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-05-01 13:36:39 -0400

    change needed to support forked ws

--- src/modules/commands/exit.js
+++ src/modules/commands/exit.js
@@ -1,6 +1,6 @@
 module.exports = function (gameServer, split) {
 
   console.log("\x1b[0m[Console] Closing server...");
-  gameServer.socketServer.close();
+  // todo we likely done need this: gameServer.socketServer.close();
   process.exit(1);
 };
