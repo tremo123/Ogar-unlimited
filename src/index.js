40b20a4e24399a8c752d48cd18f65ac3d46a86c5
Andrew54757 <andrews54757@gmail.com>
2016-05-03 18:21:54 -0400

    16.3.7

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Imports
 'use strict';
 const Readline = require('readline');
-const VERSION = '16.3.5';
+const VERSION = '16.3.7';
 const ControlServer = require('./core/ControlServer');
 let controlServer = new ControlServer(VERSION);
 //throw error
