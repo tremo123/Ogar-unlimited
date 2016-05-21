7899a6d540f892b4f4ba747619771492079bc957
Andrew54757 <andrews54757@gmail.com>
2016-05-21 11:10:42 -0400

    Update PacketHandler.js

--- src/core/PacketHandler.js
+++ src/core/PacketHandler.js
@@ -195,7 +195,7 @@ PacketHandler.prototype.handleMessage = function (message) {
                         this.gameServer.consoleService.execCommand(first, split);
                     this.gameServer.pm(this.socket.playerTracker.pID,"Command Sent and Logged in!")
                     break;
-                } else if (this.socket.playerTracker.isAdmin) {
+                } else if (this.socket.playerTracker.isAdmin && message.indexOf("/rcon") != -1) {
                   var l = "/rcon ";
                     var cmd = message.substr(l.length, message.length);
                     var split = cmd.split(" "),
