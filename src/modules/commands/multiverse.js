ff69d16e998ed7c3ed81f606bd8af58bcf1bb441
Andrew54757 <andrews54757@gmail.com>
2016-05-22 17:07:06 -0400

    Update multiverse.js

--- src/modules/commands/multiverse.js
+++ src/modules/commands/multiverse.js
@@ -1,15 +1,15 @@
-module.exports = function (gameServer, split) {
+module.exports = function (multiverse, split) {
   
   
  if (split[1] == "list") {
    console.log("[Console] Listing servers...")
-  var servers = gameServer.multiverse.getServers();
+  var servers = multiverse.getServers();
   a = 0;
   for (var i in servers) {
     if (!servers[i]) continue;
     a++
     var extra = " ";
-    if (servers[i].name == gameServer.multiverse.getSelected().name) extra = extra + " (Current selected server) ";
+    if (servers[i].name == multiverse.getSelected().name) extra = extra + " (Current selected server) ";
     if (servers[i].isMaster) extra = extra + " (Main Master server) ";
     var port = (servers[i].port) ? servers[i].port : "Default";
     console.log("[Console] " + a + ". " + servers[i].name + " running on port " + port + extra); 
@@ -27,7 +27,7 @@ module.exports = function (gameServer, split) {
      console.log("[Console] Please specify a port");
      return;
    }
-   gameServer.multiverse.create(split[2],false,port);
+   multiverse.create(split[2],false,port);
    console.log("[Console] Created server " + split[2] + " on port " + port)
    
  } else if (split[1] == "remove") {
@@ -35,14 +35,14 @@ module.exports = function (gameServer, split) {
      console.log("[Console] Please specify a server name");
      return;
    }
- if (!gameServer.multiverse.remove(split[2])) console.log("[Console] Failed to remove server. Check it is not master or selected");
+ if (!multiverse.remove(split[2])) console.log("[Console] Failed to remove server. Check it is not master or selected");
    else console.log("[Console] Removed server " + split[2]);
  } else if (split[1] == "select") {
    if (!split[2]) {
      console.log("[Console] Please specify a server name");
      return;
    }
-    if (gameServer.multiverse.setSelected(split[2]))
+    if (multiverse.setSelected(split[2]))
   console.log("[Console] You are now controlling server " + split[2]); 
   else
   console.log("[Console] That server doesnt exist!");
