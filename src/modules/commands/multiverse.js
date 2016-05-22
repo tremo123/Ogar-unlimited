9f14f9c3e7226fed86f892fce866ee1035037a2c
Andrew54757 <andrews54757@gmail.com>
2016-05-22 16:06:11 -0400

    Update multiverse.js

--- src/modules/commands/multiverse.js
+++ src/modules/commands/multiverse.js
@@ -33,8 +33,8 @@ module.exports = function (gameServer, split) {
      console.log("[Console] Please specify a server name");
      return;
    }
- if (!gameServer.multiverse.remove(split[2])) console.log("[Console] Failed to remove server. Check it is not master or selected")
-   
+ if (!gameServer.multiverse.remove(split[2])) console.log("[Console] Failed to remove server. Check it is not master or selected");
+   else console.log("[Console] Removed server " + split[2]);
  } else if (split[1] == "select") {
    if (!split[2]) {
      console.log("[Console] Please specify a server name");
