cb9a2521382f34c17d417a79c4499f22fd2482f4
Andrew54757 <andrews54757@gmail.com>
2016-05-24 13:38:24 -0400

    Update InfoServer.js

--- src/core/InfoServer.js
+++ src/core/InfoServer.js
@@ -1,9 +1,9 @@
 'use strict';
 var http = require('http');
 
-module.exports = class StatServer {
-  constructor(gameServer, port, serverStatsUpdateTime, ismaster) {
-    this.gameServer = gameServer;
+module.exports = class InfoServer {
+  constructor(multiverse, port, serverStatsUpdateTime) {
+    this.multiverse = multiverse;
     this.port = port;
     this.serverStatsUpdateTime = (serverStatsUpdateTime) ? serverStatsUpdateTime : 1;
     this.stats = "";
@@ -13,7 +13,7 @@ this.isMaster = ismaster;
 
   start() {
     // Do not start the server if the port is negative
-    if (this.port < 1 || this.gameServer.config.vps == 1 || !this.isMaster) {
+    if (this.port < 1) {
       return;
     }
     // init stats
@@ -28,7 +28,7 @@ this.isMaster = ismaster;
 
     this.httpServer.listen(this.port, function () {
       // Stats server
-      console.log("[Game] Loaded stats server on port " + this.port);
+      console.log("[Game] Loaded info server on port " + this.port);
       setInterval(this.update.bind(this), this.serverStatsUpdateTime * 1000);
     }.bind(this));
   }
@@ -42,21 +42,9 @@ this.isMaster = ismaster;
   }
 
   update() {
-    var players = 0;
-    let clients = this.gameServer.getClients();
-    clients.forEach(function (client) {
-      if (client.playerTracker && client.playerTracker.cells.length > 0)
-        players++
-    });
-
+   
     var s = {
-      'current_players': clients.length,
-      'alive': players,
-      'spectators': clients.length - players,
-      'max_players': this.gameServer.config.serverMaxConnections,
-      'gamemode': this.gameServer.gameMode.name,
-      'start_time': this.gameServer.startTime,
-      'Server_uid': this.gameServer.uid
+     
     };
     this.stats = JSON.stringify(s);
   }
